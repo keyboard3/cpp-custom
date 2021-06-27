4ea8390dc4dd5b14753d6a9a33fcc95bb89af078
甘春雨 <keyboard3@icloud.com>
2021-06-27 14:19:43 +0800

    简易栈机支持NAME和ASSIGN指令

--- virtual-stack-machine.cpp
+++ virtual-stack-machine.cpp
@@ -1,11 +1,31 @@
 #include "iostream"
+#include "vector"
 #include "stack"
+#include "map"
 using namespace std;
 
 enum OP_TYPE
 {
     NUMBER,
-    ADD
+    NAME,
+    ADD,
+    ASSIGN
+};
+enum ATOM_TYPE
+{
+    ATOM_NAME,
+    ATOM_NUMBER
+};
+struct Atom
+{
+    ATOM_TYPE flags;
+    double fval;
+    string sval;
+};
+struct Context
+{
+    vector<Atom *> atoms;
+    map<string, Atom *> globalScope;
 };
 struct CodeGenerator
 {
@@ -19,40 +39,71 @@ struct Script
 };
 CodeGenerator *NewCodeGenerator();
 Script *NewScript(CodeGenerator *cg);
-void excuteCode(Script *script);
-void generateCode(CodeGenerator *cg);
+void excuteCode(Context &kc, Script *script);
+void generateCode(Context &kc, CodeGenerator *cg);
+Atom *getAtom(Context &kc, int index);
+Atom *generateAtom(string val);
+Atom *generateAtom(double val);
 int main()
 {
+    Context kc;
     CodeGenerator *cg = NewCodeGenerator();
-    generateCode(cg);
+    generateCode(kc, cg);
     Script *script = NewScript(cg);
-    excuteCode(script);
+    excuteCode(kc, script);
 }
-void excuteCode(Script *script)
+void excuteCode(Context &kc, Script *script)
 {
     uint8_t *ptr = script->code;
     uint8_t *end = ptr + script->length;
-    stack<uint8_t> stack;
+    stack<Atom *> stack;
     while (ptr < end)
     {
         OP_TYPE op = (OP_TYPE)ptr[0];
         switch (op)
         {
         case NUMBER:
-            stack.push(ptr[1]);
+            stack.push(getAtom(kc, ptr[1]));
             ptr++;
             break;
+        case NAME:
+        {
+            Atom *nameAtom = getAtom(kc, ptr[1]);
+            if (kc.globalScope.find(nameAtom->sval) == kc.globalScope.end())
+                throw nameAtom->sval + " not defined";
+            stack.push(kc.globalScope[nameAtom->sval]);
+            ptr++;
+        }
+        break;
+        case ASSIGN:
+        {
+            Atom *nameAtom = getAtom(kc, ptr[1]);
+            Atom *valueAtom = stack.top();
+            stack.pop();
+            kc.globalScope[nameAtom->sval] = valueAtom;
+            ptr++;
+        }
+        break;
         case ADD:
-            uint8_t lval = stack.top();
+            Atom *lval = stack.top();
             stack.pop();
-            uint8_t rval = stack.top();
+            Atom *rval = stack.top();
             stack.pop();
-            stack.push(lval + rval);
+            stack.push(generateAtom(lval->fval + rval->fval));
             break;
         }
         ptr++;
     }
-    cout << (int)stack.top() << endl;
+
+    map<string, Atom *>::iterator it;
+    for (it = kc.globalScope.begin(); it != kc.globalScope.end(); ++it)
+    {
+        cout << it->first << ": ";
+        if (it->second->flags == ATOM_NUMBER)
+            cout << it->second->fval << endl;
+        else
+            cout << it->second->sval << endl;
+    }
 }
 void emit1(CodeGenerator *cg, OP_TYPE type)
 {
@@ -65,11 +116,40 @@ void emit2(CodeGenerator *cg, OP_TYPE type, uint8_t op1)
     cg->ptr[1] = op1;
     cg->ptr += 2;
 }
-void generateCode(CodeGenerator *cg)
+void generateCode(Context &kc, CodeGenerator *cg)
 {
-    emit2(cg, NUMBER, 2);
-    emit2(cg, NUMBER, 3);
+    //构建atoms
+    kc.atoms.push_back(generateAtom(2));
+    kc.atoms.push_back(generateAtom(3));
+    kc.atoms.push_back(generateAtom("a"));
+    kc.atoms.push_back(generateAtom("c"));
+    //构建指令
+    emit2(cg, NUMBER, 0);
+    emit2(cg, NUMBER, 1);
     emit1(cg, ADD);
+    emit2(cg, ASSIGN, 2);
+    emit2(cg, NAME, 2);
+    emit2(cg, NUMBER, 1);
+    emit1(cg, ADD);
+    emit2(cg, ASSIGN, 3);
+};
+Atom *getAtom(Context &kc, int index)
+{
+    return kc.atoms[index];
+}
+Atom *generateAtom(double val)
+{
+    Atom *atom = new Atom();
+    atom->flags = ATOM_NUMBER;
+    atom->fval = val;
+    return atom;
+}
+Atom *generateAtom(string val)
+{
+    Atom *atom = new Atom();
+    atom->flags = ATOM_NAME;
+    atom->sval = val;
+    return atom;
 }
 CodeGenerator *NewCodeGenerator()
 {

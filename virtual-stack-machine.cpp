4526846d48ca55ad59ff6003d17108c0ec7e33bb
甘春雨 <keyboard3@icloud.com>
2021-07-04 17:45:50 +0800

    重构指令解释

--- virtual-stack-machine.cpp
+++ virtual-stack-machine.cpp
@@ -6,141 +6,262 @@
 using namespace std;
 int main()
 {
-    Context context;
-    context.stack.ptr = context.stack.base = (Datum *)malloc(sizeof(Datum) * 1000);
-    CodeGenerator *cg = NewCodeGenerator();
-    generateCode(context, cg);
-    Script *script = NewScript(cg);
-    context.staticLink = new Object();
-    Datum *result;
-    excuteCode(&context, script, context.staticLink, result);
+  Context *context = new Context(1000);
+  generateCode(context->script);
+  Datum *result = new Datum();
+  excuteCode(context, context->script, context->staticLink, result);
+  cout << "result:" << result->u.nval << endl;
 }
-void excuteCode(Context *context, Script *script, Object *staticLink, Datum *result)
+void excuteCode(Context *context, Script *script, Object *slink, Datum *result)
 {
-    uint8_t *ptr = script->code;
-    uint8_t *end = ptr + script->length;
-    Stack &stack = context->stack;
-    while (ptr < end)
+  uint8_t *ptr = script->code;
+  uint8_t *end = ptr + script->length;
+  Stack *stack = &context->stack;
+  auto oldslink = context->staticLink;
+  context->staticLink = slink;
+  while (ptr < end)
+  {
+    OP_TYPE op = (OP_TYPE)ptr[0];
+    cout << "op:" << to_op_str(op) << endl;
+    ptr++;
+
+    switch (op)
     {
-        OP_TYPE op = (OP_TYPE)ptr[0];
-        switch (op)
-        {
-        case OP_TYPE::NAME:
-        case OP_TYPE::NUMBER:
-        {
-            Datum d;
-            d.type = DATUM_TYPE::ATOM;
-            d.u.atom = getAtom(context, ptr[1]);
-            stack.ptr[0] = d;
-            stack.ptr++;
-        }
-        break;
-        case OP_TYPE::ASSIGN:
-        {
-            Datum *rval = stack.ptr--;
-            resolveValue(context, rval);
-            Datum *lval = stack.ptr--;
-            resolveSymbol(context, lval);
-            if (lval->type == DATUM_TYPE::ATOM)
-            {
-                //构建解析符号，数据挂到自己上
-                Symobl *sym = new Symobl();
-                sym->type = SYMOBL_TYPE::VARIABLE;
-                sym->entry.key = lval->u.atom;
-                sym->entry.value = rval;
-                auto next = context->staticLink->list;
-                sym->next = next;
-                context->staticLink->list = sym;
-            }
-            else
-            {
-                //将数据覆盖到解析符号的值上
-                lval->u.sym->entry.value = rval;
-            }
-        }
-        break;
-        case OP_TYPE::ADD:
-            Datum *rval = stack.ptr--;
-            resolveValue(context, rval);
-            Datum *lval = stack.ptr--;
-            resolveValue(context, lval);
-            long long int value = lval->u.nval + rval->u.nval;
-            //将结果压入栈中
-            Datum val;
-            val.u.nval = value;
-            val.type = DATUM_TYPE::NUMBER;
-            stack.ptr[0] = val;
-            stack.ptr++;
-        }
-        ptr++;
+    case OP_TYPE::NAME:
+    case OP_TYPE::NUMBER:
+      pushAtom(getAtom(script, ptr[0]), stack);
+      ptr++;
+      break;
+    case OP_TYPE::ASSIGN:
+    {
+      Datum *lval = popDatum(stack);
+      resolveSymbol(context, lval);
+      Datum *rval = popDatum(stack);
+      resolveValue(context, rval);
+      if (lval->type == DATUM_TYPE::ATOM)
+      {
+        //构建解析符号，数据挂到自己上
+        Symbol *sym = new Symbol();
+        sym->type = SYMBOL_TYPE::VARIABLE;
+        sym->entry.key = lval->u.atom;
+        sym->entry.value = new Datum();
+        memcpy(sym->entry.value, rval, sizeof(Datum));
+        sym->slot = 1;
+
+        auto next = context->staticLink->list;
+        sym->next = next;
+        context->staticLink->list = sym;
+      }
+      else
+      {
+        //将数据覆盖到解析符号的值上
+        cout << "将数据覆盖到解析符号的值上" << lval->u.sym->entry.key->sval << endl;
+        lval->u.sym->entry.value = rval;
+      }
     }
+    break;
+    case OP_TYPE::ADD:
+    {
+      Datum *lval = popDatum(stack);
+      resolveValue(context, lval);
+      Datum *rval = popDatum(stack);
+      resolveValue(context, rval);
+      number value = lval->u.nval + rval->u.nval;
+      //将结果压入栈中
+      pushNumber(value, stack);
+    }
+    break;
+    case OP_TYPE::RETURN:
+    {
+      unsigned hasVal = ptr[0];
+      ptr++;
+      if (hasVal != 1)
+        break;
+      Datum *rval = popDatum(stack);
+      bool isOk = resolveValue(context, rval);
+      memcpy(result, rval, sizeof(Datum));
+    }
+    break;
+    }
+  }
+  context->staticLink = oldslink;
+}
+Datum *popDatum(Stack *stack)
+{
+  return --stack->ptr;
+}
+void pushAtom(Atom *atom, Stack *stack)
+{
+  Datum d;
+  d.type = DATUM_TYPE::ATOM;
+  d.u.atom = atom;
+  stack->ptr[0] = d;
+  stack->ptr++;
+}
+void pushNumber(number value, Stack *stack)
+{
+  Datum val;
+  val.u.nval = value;
+  val.type = DATUM_TYPE::NUMBER;
+  stack->ptr[0] = val;
+  stack->ptr++;
 }
 bool resolveValue(Context *context, Datum *dp)
 {
+  bool isOk = resolveSymbol(context, dp);
+  auto primaryAtomToDatum = [&]()
+  {
+    if (dp->type == DATUM_TYPE::ATOM)
+    {
+      auto atom = dp->u.atom;
+      if (atom->type == ATOM_TYPE::NUMBER)
+      {
+        dp->type = DATUM_TYPE::NUMBER;
+        dp->u.nval = atom->nval;
+        return true;
+      }
+    }
+    return false;
+  };
+  if (dp->type == DATUM_TYPE::SYMBOL)
+  {
+    auto sym = dp->u.sym;
+    if (sym->type == SYMBOL_TYPE::PROPERTY)
+    {
+      cout << "resolveValue PROPERTY" << endl;
+      //TODO 处理成从propty上拿
+      primaryAtomToDatum();
+      return true;
+    }
+    if (sym->entry.value)
+    {
+      memcpy(dp, (Datum *)sym->entry.value, sizeof(Datum));
+      //TODO 获取到的
+      primaryAtomToDatum();
+      return true;
+    }
+    cout << "准备从栈帧中获取" << endl;
+    //证明这个是从根栈帧上的符号，所以要找到scope对应的栈帧
+    if (sym->type == SYMBOL_TYPE::ARGUMENT)
+    {
+      //TODO 如果是参数，读取栈帧上的参数区域数据
+    }
+    else if (sym->type == SYMBOL_TYPE::VARIABLE)
+    {
+      //TODO 如果是变量，则读取栈帧上的变量区域数据
+    }
+    primaryAtomToDatum();
     return true;
+  }
+  else
+    return primaryAtomToDatum();
+  return false;
 }
 bool resolveSymbol(Context *context, Datum *dp)
 {
+  if (dp->type == DATUM_TYPE::ATOM)
+  {
+    auto atom = dp->u.atom;
+    if (atom->type == ATOM_TYPE::NAME)
+    {
+      auto sym = findSymbol(context->staticLink, atom);
+      if (!sym)
+        return false;
+      //只有name atom需要去作用域中找到是否存在已解析的符号
+      dp->type = DATUM_TYPE::SYMBOL;
+      dp->u.sym = sym;
+      return true;
+    }
+  }
+  else if (dp->type == DATUM_TYPE::SYMBOL)
     return true;
+  return false;
 }
-void emit1(CodeGenerator *cg, OP_TYPE type)
+Symbol *findSymbol(Object *scope, Atom *atom)
 {
-    cg->ptr[0] = (uint8_t)type;
-    cg->ptr++;
+  if (scope == nullptr)
+    return nullptr;
+  for (Symbol *sym = scope->list; sym; sym = sym->next)
+  {
+    if (sym->entry.key == atom)
+      return sym;
+  }
+  return findSymbol(scope->parent, atom);
 }
-void emit2(CodeGenerator *cg, OP_TYPE type, uint8_t op1)
+void emit1(Script *script, OP_TYPE type)
 {
-    cg->ptr[0] = (uint8_t)type;
-    cg->ptr[1] = op1;
-    cg->ptr += 2;
+  unsigned base = script->length;
+  script->code[base + 0] = (uint8_t)type;
+  script->length++;
 }
-void generateCode(Context &context, CodeGenerator *cg)
+void emit2(Script *script, OP_TYPE type, uint8_t op1)
 {
-    //构建atoms
-    context.atoms.push_back(generateAtom(2));
-    context.atoms.push_back(generateAtom(3));
-    context.atoms.push_back(generateAtom("a"));
-    context.atoms.push_back(generateAtom("c"));
-    //构建指令
-    emit2(cg, OP_TYPE::NUMBER, 0);
-    emit2(cg, OP_TYPE::NUMBER, 1);
-    emit1(cg, OP_TYPE::ADD);
-    emit2(cg, OP_TYPE::ASSIGN, 2);
-    emit2(cg, OP_TYPE::NAME, 2);
-    emit2(cg, OP_TYPE::NUMBER, 1);
-    emit1(cg, OP_TYPE::ADD);
-    emit2(cg, OP_TYPE::ASSIGN, 3);
+  unsigned base = script->length;
+  script->code[base + 0] = (uint8_t)type;
+  script->code[base + 1] = op1;
+  script->length += 2;
+}
+void generateCode(Script *script)
+{
+  //构建atoms
+  script->atoms.push_back(generateAtom(2));
+  script->atoms.push_back(generateAtom(3));
+  script->atoms.push_back(generateAtom("a"));
+  script->atoms.push_back(generateAtom("c"));
+  //构建指令
+  emit2(script, OP_TYPE::NUMBER, 0);
+  emit2(script, OP_TYPE::NUMBER, 1);
+  emit1(script, OP_TYPE::ADD);
+  emit2(script, OP_TYPE::NAME, 2);
+  emit1(script, OP_TYPE::ASSIGN);
+  emit2(script, OP_TYPE::NAME, 2);
+  emit2(script, OP_TYPE::NUMBER, 1);
+  emit1(script, OP_TYPE::ADD);
+  emit2(script, OP_TYPE::NAME, 3);
+  emit1(script, OP_TYPE::ASSIGN);
+  emit2(script, OP_TYPE::NAME, 3);
+  emit2(script, OP_TYPE::RETURN, 1);
+  cout << "code " << script->length << endl;
 };
-Atom *getAtom(Context *context, int index)
+Atom *getAtom(Script *script, int index)
 {
-    return context->atoms[index];
+  return script->atoms[index];
 }
-Atom *generateAtom(double val)
+Atom *generateAtom(number val)
 {
-    Atom *atom = new Atom();
-    atom->type = ATOM_TYPE::NUMBER;
-    atom->fval = val;
-    return atom;
+  Atom *atom = new Atom();
+  atom->type = ATOM_TYPE::NUMBER;
+  atom->nval = val;
+  return atom;
 }
 Atom *generateAtom(string val)
 {
-    Atom *atom = new Atom();
-    atom->type = ATOM_TYPE::NAME;
-    atom->sval = val;
-    return atom;
+  Atom *atom = new Atom();
+  atom->type = ATOM_TYPE::NAME;
+  atom->sval = val;
+  return atom;
 }
-CodeGenerator *NewCodeGenerator()
+void dumpScope(Object *scope)
 {
-    CodeGenerator *cg = new CodeGenerator();
-    //暂时分配1000代码缓存区
-    cg->ptr = cg->base = (uint8_t *)malloc(sizeof(uint8_t) * 1000);
-    return cg;
+  for (Symbol *sym = scope->list; sym; sym = sym->next)
+  {
+    Datum *temp = (Datum *)sym->entry.value;
+    cout << sym->entry.key->sval << " " << temp->u.nval << endl;
+  }
 }
-Script *NewScript(CodeGenerator *cg)
+string to_op_str(OP_TYPE op)
 {
-    Script *script = new Script();
-    script->length = cg->ptr - cg->base;
-    script->code = (uint8_t *)malloc(sizeof(uint8_t) * script->length);
-    memcpy(script->code, cg->base, script->length);
-    return script;
+  switch (op)
+  {
+  case OP_TYPE::NUMBER:
+    return "number";
+  case OP_TYPE::NAME:
+    return "name";
+  case OP_TYPE::ADD:
+    return "add";
+  case OP_TYPE::ASSIGN:
+    return "assign";
+  case OP_TYPE::RETURN:
+    return "return";
+  }
 }
--- virtual-stack-machine.h
+++ virtual-stack-machine.h
@@ -3,12 +3,14 @@
 #include "vector"
 #include "stack"
 using namespace std;
+typedef unsigned long number;
 enum class OP_TYPE
 {
     NUMBER,
     NAME,
     ADD,
-    ASSIGN
+    ASSIGN,
+    RETURN
 };
 enum class ATOM_TYPE
 {
@@ -19,20 +21,20 @@ struct Atom
 {
     ATOM_TYPE type;
     double fval; //TODO 删除
-    long long int nval;
+    number nval;
     string sval;
 };
-enum class SYMOBL_TYPE
+enum class SYMBOL_TYPE
 {
     ARGUMENT,
     VARIABLE,
     PROPERTY
 };
-struct Symobl
+struct Symbol
 {
-    SYMOBL_TYPE type;
+    SYMBOL_TYPE type;
     uint8_t slot;
-    Symobl *next;
+    Symbol *next;
     struct Entry
     {
         Atom *key;
@@ -55,7 +57,7 @@ struct Datum
         long long int nval;
         string sval;
         Atom *atom;
-        Symobl *sym;
+        Symbol *sym;
     } u;
 };
 struct CodeGenerator
@@ -63,17 +65,31 @@ struct CodeGenerator
     uint8_t *base; //指令的起始地址
     uint8_t *ptr;  //指令移动指针
 };
-struct Script
+class Script
 {
+public:
+    Script(unsigned codeLimit)
+    {
+        code = (uint8_t *)malloc(sizeof(uint8_t) * codeLimit);
+        args = nullptr;
+        argc = 0;
+        length = 0;
+    }
     vector<Atom *> atoms;
-    Symobl *args;    //参数符号
+    Symbol *args;    //参数符号
     uint8_t argc;    //参数数量
     uint8_t *code;   //代码
     unsigned length; //代码长度
 };
-struct Object
+class Object
 {
-    Symobl *list; //符号链
+public:
+    Object()
+    {
+        list = nullptr;
+        parent = nullptr;
+    }
+    Symbol *list; //符号链
     Object *parent;
 };
 struct Stack
@@ -81,18 +97,30 @@ struct Stack
     Datum *base; //数据栈
     Datum *ptr;  //指向最近的数据栈
 };
-struct Context
+class Context
 {
-    vector<Atom *> atoms;            //TODO 删除
-    map<string, Atom *> globalScope; //TODO 删除
-    Script *script;                  //根代码
-    Stack stack;                     //数据栈
-    Object *staticLink;              //顶层的作用域
+public:
+    Context(int stackSize)
+    {
+        script = new Script(1000);
+        staticLink = nullptr;
+        stack.ptr = stack.base = (Datum *)malloc(sizeof(Datum) * stackSize);
+        staticLink = new Object();
+    }
+    Script *script;     //根代码
+    Stack stack;        //数据栈
+    Object *staticLink; //顶层的作用域
 };
-CodeGenerator *NewCodeGenerator();
-Script *NewScript(CodeGenerator *cg);
-void excuteCode(Context *kc, Script *script, Object *staticLink, Datum *result);
-void generateCode(Context *kc, CodeGenerator *cg);
-Atom *getAtom(Context *kc, int index);
+Symbol *findSymbol(Object *scope, Atom *atom);
+void excuteCode(Context *kc, Script *script, Object *slink, Datum *result);
+void generateCode(Script *script);
+void pushAtom(Atom *atom, Stack *stack);
+void pushNumber(number value, Stack *stack);
+Datum *popDatum(Stack *stack);
+bool resolveSymbol(Context *context, Datum *dp);
+bool resolveValue(Context *context, Datum *dp);
+Atom *getAtom(Script *script, int index);
 Atom *generateAtom(string val);
-Atom *generateAtom(double val);
+Atom *generateAtom(number val);
+string to_op_str(OP_TYPE op);
+void dumpScope(Object *scope);

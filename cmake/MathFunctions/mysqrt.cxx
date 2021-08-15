f304fb31ba1a12f806b018327f99c706dd6a9e16
甘春雨 <keyboard3@icloud.com>
2021-08-15 21:54:49 +0800

    add: adding System Introspection

--- cmake/MathFunctions/mysqrt.cxx
+++ cmake/MathFunctions/mysqrt.cxx
@@ -1,22 +1,33 @@
 #include <iostream>
+#include <cmath>
 
 // 使用简单的操作实现平方根的计算
 double mysqrt(double x)
 {
-  if (x <= 0) {
+  if (x <= 0)
+  {
     return 0;
   }
 
+//如果系统提供了log和exp函数，直接使用它们
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = exp(log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+#else
   double result = x;
 
   // do ten iterations
-  for (int i = 0; i < 10; ++i) {
-    if (result <= 0) {
+  for (int i = 0; i < 10; ++i)
+  {
+    if (result <= 0)
+    {
       result = 0.1;
     }
     double delta = x - (result * result);
     result = result + 0.5 * delta / result;
     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
   }
+#endif
   return result;
 }

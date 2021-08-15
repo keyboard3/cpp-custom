6b30a523341bdad5288cc084dfaeef9c037528b9
甘春雨 <keyboard3@icloud.com>
2021-08-15 21:13:19 +0800

    add: 添加cmake的实验到step4

---
+++ cmake/tutorial.cxx
@@ -0,0 +1,35 @@
+// 一个计算数字平方根的简单程序
+#include <cmath>
+#include <iostream>
+#include <string>
+#include "TutorialConfig.h"
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
+int main(int argc, char *argv[])
+{
+  //没参数，就打印程序的版本
+  if (argc < 2)
+  {
+    //argv[0]是程序名称，包含了程序所在的路径
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  }
+
+  //有参数就将输入转成double c++11的特性(string)
+  const double inputValue = std::stod(argv[1]);
+
+//如果编译指定使用自己平方根库
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
+  //计算输入的数字的平方根
+  const double outputValue = sqrt(inputValue);
+#endif
+  std::cout << "The square root of " << inputValue << " is " << outputValue
+            << std::endl;
+  return 0;
+}

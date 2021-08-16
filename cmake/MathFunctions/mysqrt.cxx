ad85db08a379518c7730184f6d12dfd0959b6a43
甘春雨 <keyboard3@icloud.com>
2021-08-16 09:25:35 +0800

    add: Selecting Static or Shared Libraries

--- cmake/MathFunctions/mysqrt.cxx
+++ cmake/MathFunctions/mysqrt.cxx
@@ -2,7 +2,8 @@
 // #include <cmath>
 // 引入这个自动生成的头文件，包含了sqrtTable已经计算好的table
 #include "Table.h"
-
+namespace mathfunctions {
+namespace detail {
 // 使用简单的操作实现平方根的计算
 double mysqrt(double x) {
   if (x <= 0) {
@@ -27,23 +28,26 @@ double mysqrt(double x) {
   }
 
   return result;
-//如果系统提供了log和exp函数，直接使用它们
-// #if defined(HAVE_LOG) && defined(HAVE_EXP)
-//   double result = exp(log(x) * 0.5);
-//   std::cout << "Computing sqrt of " << x << " to be " << result
-//             << " using log and exp" << std::endl;
-// #else
-//   double result = x;
+  //如果系统提供了log和exp函数，直接使用它们
+  // #if defined(HAVE_LOG) && defined(HAVE_EXP)
+  //   double result = exp(log(x) * 0.5);
+  //   std::cout << "Computing sqrt of " << x << " to be " << result
+  //             << " using log and exp" << std::endl;
+  // #else
+  //   double result = x;
 
-//   // do ten iterations
-//   for (int i = 0; i < 10; ++i) {
-//     if (result <= 0) {
-//       result = 0.1;
-//     }
-//     double delta = x - (result * result);
-//     result = result + 0.5 * delta / result;
-//     std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
-//   }
-// #endif
-//   return result;
+  //   // do ten iterations
+  //   for (int i = 0; i < 10; ++i) {
+  //     if (result <= 0) {
+  //       result = 0.1;
+  //     }
+  //     double delta = x - (result * result);
+  //     result = result + 0.5 * delta / result;
+  //     std::cout << "Computing sqrt of " << x << " to be " << result <<
+  //     std::endl;
+  //   }
+  // #endif
+  //   return result;
 }
+} // namespace detail
+} // namespace mathfunctions
